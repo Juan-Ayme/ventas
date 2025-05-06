ed8085200eac1061616562ef9124d04282565f51
Juan Ayme <juanayme565@gmail.com>
2025-05-06 14:20:18 -0500

    Inicio del proyecto subev2

--- venta_sistema/settings.py
+++ venta_sistema/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,10 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-#2spdb+d63eb(+-0wvn(5c4ma9($!j3@^spe5@&801tdx342yg'
-SECRET_KEY = os.environ.get('SECRET_KEY', default= "llave")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -74,8 +73,6 @@ WSGI_APPLICATION = 'venta_sistema.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-
 
 DATABASES = {
     'default': {
